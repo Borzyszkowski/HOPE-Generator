38d71c941b85069577a9f5fe9890534089473bf1
bartlomiej.borzyszkowski@epfl.ch <bartlomiej.borzyszkowski@epfl.ch>
2022-11-09 15:17:48 +0100

    updated training

--- training/trainer.py
+++ training/trainer.py
@@ -1,6 +1,5 @@
 """ Implementation of the main training logic """
 
-import numpy as np
 import torch
 from torchvision.utils import make_grid
 
@@ -48,33 +47,35 @@ class Trainer(BaseTrainer):
         for batch_idx, (data, target) in enumerate(self.data_loader):
             data, target = data.to(self.device), target.to(self.device)
 
+            # run backpropagation
             self.optimizer.zero_grad()
             output = self.model(data)
             loss = self.criterion(output, target)
             loss.backward()
             self.optimizer.step()
 
+            # save metrics to the tensorboard
             self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.train_metrics.update('loss', loss.item())
             for met in self.metric_ftns:
                 self.train_metrics.update(met.__name__, met(output, target))
 
+            # print the loss message
             if batch_idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} Iter: {} Progress: {} Loss: {:.6f}'.format(
-                    epoch,
-                    batch_idx,
-                    self._progress(batch_idx),
-                    loss.item()))
+                self._iter_message('Train', epoch, batch_idx, loss)
                 self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
 
+            # break the epoch when the whole data is used
             if batch_idx == self.len_epoch:
                 break
-        log = self.train_metrics.result()
 
+        # run evaluation on the validation set
+        log = self.train_metrics.result()
         if self.do_validation:
             val_log = self._valid_epoch(epoch)
             log.update(**{'val_' + k: v for k, v in val_log.items()})
 
+        # update the learning rate scheduler
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
         return log
@@ -92,28 +93,33 @@ class Trainer(BaseTrainer):
             for batch_idx, (data, target) in enumerate(self.valid_data_loader):
                 data, target = data.to(self.device), target.to(self.device)
 
+                # compute loss
                 output = self.model(data)
                 loss = self.criterion(output, target)
 
+                # save metrics to the tensorboard
                 self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
                 self.valid_metrics.update('loss', loss.item())
                 for met in self.metric_ftns:
                     self.valid_metrics.update(met.__name__, met(output, target))
                 self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
 
+                # print the loss message
                 if batch_idx % self.log_step == 0:
-                    self.logger.debug('Val Epoch: {} Iter: {} Progress: {} Loss: {:.6f}'.format(
-                        epoch,
-                        batch_idx,
-                        self._progress(batch_idx),
-                        loss.item()))
+                    self._iter_message('Val', epoch, batch_idx, loss)
 
         # add histogram of model parameters to the tensorboard
         for name, p in self.model.named_parameters():
             self.writer.add_histogram(name, p, bins='auto')
         return self.valid_metrics.result()
 
+    def _iter_message(self, mode, epoch, batch_idx, loss):
+        """ Generate the message after each interation """
+        progress = self._progress(batch_idx)
+        self.logger.debug(f'{mode} Epoch: {epoch} Iter: {batch_idx} Progress: {progress} Loss: {loss.item():.6f}')
+
     def _progress(self, batch_idx):
+        """ Show the training progress of each epoch """
         base = '[{}/{} ({:.0f}%)]'
         if hasattr(self.data_loader, 'n_samples'):
             current = batch_idx * self.data_loader.batch_size
