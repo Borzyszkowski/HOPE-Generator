0bf763261068ea7e768d6e8c637471fda48fc19d
borzyszkowski <bartek_borzyszkowksi@wp.pl>
2022-11-20 17:08:56 +0100

    support for GRAB

---
+++ datasets/grab/grab_utils.py
@@ -0,0 +1,17 @@
+""" General purpose utility functions for GRAB dataset """
+
+# colors used for visualizations
+colors = {
+    'pink': [1.00, 0.75, 0.80],
+    'skin': [0.96, 0.75, 0.69],
+    'purple': [0.63, 0.13, 0.94],
+    'red': [1.0, 0.0, 0.0],
+    'green': [.0, 1., .0],
+    'yellow': [1., 1., 0],
+    'brown': [1.00, 0.25, 0.25],
+    'blue': [.0, .0, 1.],
+    'white': [1., 1., 1.],
+    'orange': [1.00, 0.65, 0.00],
+    'grey': [0.75, 0.75, 0.75],
+    'black': [0., 0., 0.],
+}
