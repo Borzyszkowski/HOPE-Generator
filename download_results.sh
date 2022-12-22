b2e345ab4e6e6d680643b8d47c5ff61e0e0d63f3
Mirali Ahmadli <miraliahmadli@gmail.com>
2022-12-22 01:25:48 +0100

    Update results folder

--- download_results.sh
+++ download_results.sh
@@ -3,10 +3,10 @@
 mkdir -p ./_RESULTS/Downloaded
 cd ./_RESULTS/Downloaded
 
-gdown "https://drive.google.com/uc?id=1kFK1bb4F_rGJ0bkEuSGdVjLTecqU__8o"
-unzip 100_objects_meshes.zip
-rm 100_objects_meshes.zip
+gdown "https://drive.google.com/uc?id=1fpyv5e39F5TQGzi1sCG4wPLBzK91ViG4"
+unzip objects_meshes.zip
+rm objects_meshes.zip
 
-gdown "https://drive.google.com/uc?id=1idFs92EU3JswTtUPopBmisSKWaFnw6bl"
-unzip 100_objects_visualized.zip
-rm 100_objects_visualized.zip
+gdown "https://drive.google.com/uc?id=146DfXp61WYMA23CfK8z4jqytAXlQVX9d"
+unzip objects_visualized.zip
+rm objects_visualized.zip
